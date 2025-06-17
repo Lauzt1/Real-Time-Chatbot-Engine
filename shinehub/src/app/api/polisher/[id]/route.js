59249d2fe98779cb183f1e07a4936c92756c4562
Wei Joe <joe222666@gmail.com>
2025-06-18 02:36:45 +0800

    tidy up some codes

--- shinehub/src/app/api/polisher/[id]/route.js
+++ shinehub/src/app/api/polisher/[id]/route.js
@@ -16,10 +16,10 @@ export async function PUT(request, { params }) {
     } = await request.json();
     await connectMongoDB();
     await Polisher.findByIdAndUpdate(id, { name, backingpad, orbit, power, rpm, weight, description, imageUrl });
-    return NextResponse.json({ message: "Polisher Added" }, { status: 200 });
+    return NextResponse.json({ message: "Polisher updated" }, { status: 200 });
 }
 
-export async function GET({ params }) {
+export async function GET({ request, params }) {
     const { id } = params;
     await connectMongoDB();
     const polisher = await Polisher.findOne({ _id: id });
